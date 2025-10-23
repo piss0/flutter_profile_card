9d1065edd612ca80213fb18290fbde262dda6920
piss0 <hafizrisk425@gmail.com>
2025-10-23 14:21:03 +0700

    belajar decoration dan container

--- lib/profile.dart
+++ lib/profile.dart
@@ -8,31 +8,79 @@ class profilecard extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.redAccent,
-        title: const Text('my profile',
-        style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-        ),),
+        title: const Text(
+          'My Profile',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('hello world',style: TextStyle(fontSize: 20,
-            fontWeight: FontWeight.bold,
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8, //
+              height: 300,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.amber,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.redAccent,
+                    spreadRadius: 5.0,
+                    blurRadius: 7.0,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+             Padding(
+                    padding: const EdgeInsets.only(bottom:  10),
+                    /**
+                     * only : hanya memberi satu posisi
+                     * symmetric : memberi jarak sumbu x/y
+                     * all: memberi semua jarak
+                     */
+                    child: Text(
+                      'Nama',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Arkham Helga',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.5,
+                            decoration: BoxDecoration(color: Colors.redAccent,
+                            borderRadius: BorderRadius.circular(
+                              15,
+                            )
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text('Address'),
+                            ),
+                            ),
+                        ],
+                      ),
+                      Column(),
+                    ],
+                  ),
+                ],
+              ),
             ),
-           ),
-            Row(
-              children: <Widget>[
-                Column(
-                  children: <Widget>[],
-                ),
-                Column(),
-              ],
-            )
           ],
         ),
-      ),
-    );
-  }
+    ),
+);
 }
-//codingan profile card
+}
